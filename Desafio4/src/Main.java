74eee66d1f41129932cd59794d858cade6232689
PedroHaratani <76952955+PedroHaratani@users.noreply.github.com>
2022-12-19 19:17:36 -0300

    Desafio 4 completo
    Desafio 4 finalizado

--- Desafio4/src/Main.java
+++ Desafio4/src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner in = new Scanner(System.in);
+        String str=in.nextLine();
+
+
+        if(str.length()>1 && str.length()<=100) {
+            if (str.length() % 2 == 0) {
+                String str1 = str.substring(0, str.length() / 2);
+                StringBuilder strBuilder = new StringBuilder(str1);
+                str1 = strBuilder.reverse().toString();
+                String str2 = str.substring(str.length() / 2, str.length());
+                strBuilder = new StringBuilder(str2);
+                str2 = strBuilder.reverse().toString();
+
+                System.out.println(str1 + str2);
+            }
+        } else {
+            System.out.println("Essa string não é válida");
+        }
     }
 }
