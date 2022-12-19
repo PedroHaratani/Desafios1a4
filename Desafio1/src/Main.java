279897095b6195057ffe0283ce0e91663b1a1c56
PedroHaratani <76952955+PedroHaratani@users.noreply.github.com>
2022-12-19 20:25:07 -0300

    Ajuste no desafio 1
    Ajustes no desafio 1

--- Desafio1/src/Main.java
+++ Desafio1/src/Main.java
@@ -5,12 +5,22 @@ public class Main {
     public static void main(String[] args) {
         int n,nums;
         Scanner in = new Scanner(System.in);
+        System.out.println("Insira o numero de itens a serem inseridos");
         n=in.nextInt();
+        if(n<0 && n>100000){
+            System.out.println("Insira um numero maior que zero ou menor que 100000");
+            n=in.nextInt();
+        }
         List<Integer> numerosPares = new ArrayList<>();
         List<Integer> numerosImpares = new ArrayList<>();
 
         for(int i = 0; i < n; i++){
+            System.out.println("Insira um valor");
             nums=in.nextInt();
+            if(nums<0){
+                System.out.println("Insira um valor não negativo");
+                nums=in.nextInt();
+            }
             if(nums%2==0){
                 numerosPares.add(nums);
             }else {
